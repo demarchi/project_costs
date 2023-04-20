5426c35f908ecf2f45457a7a8a813ea4b46c06e2
demarchi <bruno1549@gmail.com>
2023-04-20 21:36:35 +0100

    Inserting data into the database via API

--- src/componets/Project/ProjectForm.js
+++ src/componets/Project/ProjectForm.js
@@ -5,10 +5,12 @@ import Select from '../form/Select'
 import LinkButton from '../layout/LinkButton'
 
 import { useState, useEffect } from 'react'
+import SubmitButton from '../layout/SubmitButton'
 
-function ProjectForm({ btnText }) {
+function ProjectForm({ handleSubmit, btnText, projectData }) {
 
   const [categories, setCategories] = useState([])
+  const [project, setProject] = useState( projectData || {} )
 
   useEffect(() => {
     fetch( "http://localhost:5000/categories", {
@@ -24,26 +26,52 @@ function ProjectForm({ btnText }) {
     .catch((err) => console.log(err))
   }, [])
 
+  const submit = (e) => {
+    e.preventDefault()
+    // console.log("submit: ", project)
+    handleSubmit(project)
+  }
+
+  function handleChange(e) {
+    setProject({ ...project, [e.target.name]: e.target.value })
+    // console.log("handleChange: ", project)
+    
+  }
+
+  function handleCategory(e) {
+    setProject({ ...project, category: {
+      id: e.target.value,
+      name: e.target.options[e.target.selectedIndex].text,
+    } })
+    // console.log("handleCategory: ", project)
+  }
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={submit} className={styles.form}>
         <Input 
             type='text'
             text='Nome do Projeto'
             name='name'
             placeholder='Insira o nome do Projeto'
+            handleOnChange={handleChange}
+            value={project.name ? project.name : '' }
         />
         <Input 
             type='number'
             text='Orçamento do Projeto'
             name='budget'
             placeholder='Insira o orçamento do Projeto'
+            handleOnChange={handleChange}
+            value={project.budget ? project.budget : '' }
         />
         <Select 
           name='category_id' 
           text='Selecione a categoria' 
           options={categories}
+          handleOnChange={handleCategory}
+          value={project.category ? project.category.id : ''}
         />
-        <LinkButton text={btnText} to='/' />
+        <SubmitButton text={btnText} />
     </form>
   )
 }
--- src/componets/form/Select.js
+++ src/componets/form/Select.js
@@ -6,7 +6,12 @@ function Select({ text, name, options, handleOnChange, value }) {
   return (
     <div className={styles.input_control}>
         <label htmlFor={name}>{text}</label>
-        <select name={name} id={name}>
+        <select 
+          name={name} 
+          id={name} 
+          onChange={handleOnChange} 
+          value={value || ''}
+        >
             <option>Selecione uma opção</option>
             {
               options.map((option) => (
